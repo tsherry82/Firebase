43d14c1f59a5796be589befd6ed7911f8b680fa0
Tim Sherry <tsherry82@gmail.com>
2019-07-19 19:37:11 -0400

    linked database and worked on linking inputs to database

---
+++ assets/js/app.js
@@ -0,0 +1,45 @@
+// **********SUDO COODE**********
+console.log("test")
+
+// **********DATABASE**********
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA8Yb2Cn8rpighS0J6UVvfhpS4yOrIS9Ng",
+    authDomain: "train-schedule-e5613.firebaseapp.com",
+    databaseURL: "https://train-schedule-e5613.firebaseio.com",
+    projectId: "train-schedule-e5613",
+    storageBucket: "",
+    messagingSenderId: "424758889484",
+    appId: "1:424758889484:web:1a7bb953fcdceb16"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// **********VARIABLES*********
+
+
+var database = firebase.database();
+
+$("#submit").on("click", function () {
+
+    var newTrainName = $("#train-name-input").val();
+    var newDestination = $("#destination-input").val();
+    var newTrainTime = $("#train-time-input").val();
+    var frequency = $("#frequency-input").val();
+
+    var tempTrain = {
+        name: newTrainName,
+        destination: newDestination,
+        time: newTrainTime,
+        frequency: frequency,
+    };
+
+    database.ref().push(tempTrain)
+
+    $("#train-name-input").val("")
+    $("#destination-input").val("")
+    $("#train-time-input").val("")
+    $("#frequency-input").val("")
+});
+
